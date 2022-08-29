7339f9786d3d1c04d2254558ec8c65a0555b8a8d
Hiba <eng.lilil92@gmail.com>
2022-08-29 18:04:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML=" Hiba is the champion";
+document.body.append(newHeader);
+//newHeader.textContent = "h1#victory";
+// newHeader.append("body");
+//newHeader.setAttribute("id","victory");
+
+//main.setAttribute("id","victory");
